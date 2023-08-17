f5048b70cd6cb01ca6bd7f822e945a836ec637f2
kundan kumar <kundanchauhan1997@gmail.com>
2023-08-17 14:11:23 +0530

    ci/cd publish

--- FundooNotes/Entity/Program.cs
+++ FundooNotes/Entity/Program.cs
@@ -75,11 +75,9 @@ namespace FundooNotes.Entity
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+          
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
